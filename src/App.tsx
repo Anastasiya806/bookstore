132c77e38bcd120856599f27103ee475cffe4b2b
Anastasiya806 <96192476+Anastasiya806@users.noreply.github.com>
2022-06-05 22:49:55 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./header/Header";
 import Content from "./main/Content";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+      <>
+        <div className="footer-wrapper">
+          <p className="footer-title">Subscribe to Newsletter</p>
+          <p className="footer-subtitle">
+            Be the first to know about new IT books, upcoming releases,
+            exclusive offers and more.
+          </p>
+          <div className="footer-input">
+            <input placeholder="Your email"></input>
+            <button className="footer-button">Subscribe</button>
+          </div>
+        </div>
+        <div className="footer-info">
+          <p>©2022 Bookstore</p>
+          <p>All rights reserved</p>
+        </div>
+      </>
+    );
+}
+
+export default Footer;
