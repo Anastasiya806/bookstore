b5624066e5057ea45fa28baf77e68f5abbeb56c6
Anastasiya806 <96192476+Anastasiya806@users.noreply.github.com>
2022-06-05 19:19:20 +0300

    add header

---
+++ src/header/Header.tsx
@@ -0,0 +1,20 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <p className='header-title'>BOOKSTORE</p>
+      <div className='header-input'>
+        <input placeholder="Search" />
+        <div className='search'></div>
+      </div>
+      <div className="icons">
+        <div className="like svg"></div>
+        <div className="shop svg"></div>
+        <div className="user svg"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
